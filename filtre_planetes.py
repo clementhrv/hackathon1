2519c779e7d792b506c167e7d0bb0b9eaa56815c
Clément Hervé <clement.herve@etu.minesparis.psl.eu>
2023-10-31 18:50:04 +0100

    commentaires

--- filtre_planetes.py
+++ filtre_planetes.py
@@ -12,7 +12,7 @@ df2 = pd.read_csv('cumulative_2023.10.31_07.12.57.csv', skiprows=96)
 
 columns1 = ['Planet Name','Host Name', 'Default Parameter Set', 'Number of Stars','Number of Planets','Discovery Method','Discovery Year','Solution Type','Controversial Flag','Planetary Parameter Reference','Orbital Period','Orbit Semi-Major Axis','Planet Radius [Earth Radius]','Planet Radius [Jupiter Radius]','Planet Mass or Mass*sin(i) [Earth Mass]','Planet Mass or Mass*sin(i) [Jupiter Mass]','Planet Mass or Mass*sin(i) Provenance','Eccentricity','Insolation Flux','Equilibrium Temperature','Data Show Transit Timing Variations','Stellar Parameter Reference','Spectral Type','Stellar Effective Temperature','Stellar Radius ','Stellar Mass','Stellar Metallicity','Stellar Metallicity Ratio','Stellar Surface Gravity']
 
-
+#on filtre à la main pour ne garder que les colonnes utiles 
 columns1_inutiles = [ 'pl_orbpererr1',
        'pl_orbpererr2',  'pl_orbsmaxerr1',
        'pl_orbsmaxerr2',  'pl_radeerr1',
@@ -74,4 +74,5 @@ df1 = df1[df1['Equilibrium Temperature [K]']>0]
 #df1 = df1[293<df1['Equilibrium Temperature [K]']<353]
 #df = df[0.1>df['Eccentricity']>0.067]
 #df =df[10>df ['Planet Mass Mass*sin(i)']>1]
-df1['temp_prc'] = df1['']"""
+df1['temp_prc'] = df1['']
+"""
