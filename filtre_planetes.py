664fa9f08fc6accee50ff8ef577dd2fef027c80e
Clément Hervé <clement.herve@etu.minesparis.psl.eu>
2023-10-31 15:15:58 +0100

    avancé de la récupération des données

--- filtre_planetes.py
+++ filtre_planetes.py
@@ -1,5 +1,10 @@
 import pandas as pd 
 import numpy as np 
 
-df = pd.read_csv('toute_planetes.csv', skiprows=291)
-print(df)
+#importation de la data avec toutes les données
+df1 = pd.read_csv('toute_planetes.csv', skiprows=291)
+
+columns = ['Planet Name','Host Name', 'Defaull Parameter Set', 'Number']
+
+
+
